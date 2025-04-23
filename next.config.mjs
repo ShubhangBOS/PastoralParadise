8edf67448bd7b58452776079a2caae5421ab27cb
Mayank Bos <mayank@boscenter.in>
2025-04-23 12:15:59 +0530

    commented non use env varialble

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,11 @@
 const nextConfig = {
   devIndicators: false,
   reactStrictMode: false,
-  env: {
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dctahvizk",
-    NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN:
-      "pk.eyJ1Ijoia29vbGtpc2hhbiIsImEiOiJjazV3Zm41cG8wa3I1M2tydnVkcW53b2ZpIn0.mYrXogbdTrWSoJECNR1epg",
-  },
+  // env: {
+  //   NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dctahvizk",
+  //   NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN:
+  //     "pk.eyJ1Ijoia29vbGtpc2hhbiIsImEiOiJjazV3Zm41cG8wa3I1M2tydnVkcW53b2ZpIn0.mYrXogbdTrWSoJECNR1epg",
+  // },
   images: {
     remotePatterns: [
       {
